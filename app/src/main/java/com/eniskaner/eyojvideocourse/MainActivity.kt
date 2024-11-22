cc77724f4dd20bfc7615a0f303c2f2465f2a52d5
Enis Kaner <101512938+EyoJneSinE@users.noreply.github.com>
2024-11-22 03:08:58 +0300

    add Feature/setupactivity merge into dev (#2)
    * add nav controller
    
    * add hilt into project for dependency injection
    
    * add setUpWindowInsets func
    
    * add setupSystemBars func about full screen mode
    
    * add nav controller and nav graph into MainActivity

--- app/src/main/java/com/eniskaner/eyojvideocourse/MainActivity.kt
+++ app/src/main/java/com/eniskaner/eyojvideocourse/MainActivity.kt
@@ -4,16 +4,65 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavController
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.NavHostFragment
+import com.eniskaner.eyojvideocourse.databinding.ActivityMainBinding
+import com.eniskaner.navigationcourseapp.NavigationGraph
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var navigationGraph: @JvmSuppressWildcards Set<NavigationGraph>
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+
+        setupWindowInsets()
+        setupSystemBars()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupNavGraph()
+    }
+
+    private fun setupNavGraph() {
+        navController.graph = navController.createGraph(
+            startDestination = "",
+        ) {
+            navigationGraph.forEach { navNodes ->
+                navNodes.addNav(this)
+            }
+        }
+    }
+
+    private fun setupSystemBars() {
+        window.let { window ->
+            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+            windowInsetsController.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
