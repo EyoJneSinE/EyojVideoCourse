f846b90fa0113274f2517ce5122b2f01647889ee
Enis Kaner <enis.d.kaner@gmail.com>
2024-11-28 15:30:28 +0300

    add push notifications with one signal

--- app/src/main/java/com/eniskaner/eyojvideocourse/CourseApplication.kt
+++ app/src/main/java/com/eniskaner/eyojvideocourse/CourseApplication.kt
@@ -1,7 +1,29 @@
 package com.eniskaner.eyojvideocourse
 
 import android.app.Application
+import com.eniskaner.eyojvideocourse.OneSignalConstants.ONESIGNAL_APP_ID
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
-class CourseApplication : Application()
+class CourseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID)
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(true)
+        }
+    }
+}
